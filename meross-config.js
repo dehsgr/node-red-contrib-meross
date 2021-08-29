0ce88758d66233ed22aaa8b6e9610868c424aa72
dehsgr <dehsgr@outlook.com>
2021-08-29 15:26:42 +0200

    Worked on issue #11.

---
+++ meross-config.js
@@ -0,0 +1,14 @@
+module.exports = function(RED) {
+	function MerossNode(MyNode) {
+		RED.nodes.createNode(this, MyNode);
+		this.messageid = MyNode.messageid;
+		this.token = MyNode.token;
+		this.timestamp = MyNode.timestamp;
+	}
+
+	RED.nodes.registerType("meross-config", MerossNode, {
+		messageid: { type: "text" },
+		token: { type: "text" },
+		timestamp: { type: "text" }
+	});
+};
---
+++ smartgarage-control.js
@@ -0,0 +1,73 @@
+const request = require('request');
+
+module.exports = function(RED) {
+	'use strict';
+
+	function SmartGarageNode(myNode) {
+		RED.nodes.createNode(this, myNode);
+		var Platform = this;
+
+		this.config = RED.nodes.getNode(myNode.confignode);
+		this.ip = myNode.ip;
+		this.channel = parseInt(myNode.channel || 0);
+
+		this.on('input', function (msg) {
+			request.post({
+				url: 'http://' + Platform.ip + '/config',
+				headers: {
+					'Content-Type': 'application/json'
+				}, 
+				body: JSON.stringify({
+					'header': {
+						'messageId': Platform.config.messageid,
+						'method': (typeof msg.payload === 'boolean') ?
+								  'SET' :
+								  'GET',
+						'from': 'http://' + Platform.ip + '/config',
+						'sign': Platform.config.token,
+						'namespace': (typeof msg.payload === 'boolean') ?
+									 'Appliance.GarageDoor.State' :
+									 'Appliance.System.All',
+						'timestamp': parseInt(Platform.config.timestamp),
+						'payloadVersion': 1
+					},
+					'payload': (typeof msg.payload === 'boolean') ?
+					 {
+						'state': {
+							'open': msg.payload ? 1 : 0,
+							'channel': Platform.channel,
+							'uuid': makeUUID(16)
+						}
+					} : 
+					{}
+				})
+			}, function(myError, myResponse, myBody) {
+				if(myError) {
+					Platform.warn('There was an Error: ' + myError);
+				} else {
+					var j = JSON.parse(myResponse.body);
+					try {
+						var r = (j.header.method !== undefined && j.header.method === 'SETACK') ?
+								msg.payload :
+								j.payload.all.digest.garageDoor[Platform.channel].open === 1 ? true : false;
+					}
+					catch (e) {
+						var r = 'Received unexpected data!';
+					}
+					Platform.send({ payload : r });
+				}
+			});	   
+		});
+	}
+	
+	function makeUUID(size) {
+		let uuid = '';
+		const chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
+		while (uuid.length < size) {
+			uuid += chars.charAt(Math.floor(Math.random() * chars.length));
+		}
+		return uuid;
+	 }
+
+	RED.nodes.registerType('smartgarage-control', SmartGarageNode);
+};
